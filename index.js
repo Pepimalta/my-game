7fb4e4a9ba84cc3d22d30fb21bbb5246c4a0162a
Pepimalta <vitormaltafurtado@gmail.com>
2026-07-09 20:13:21 -0300

    Add canvas drawing functionality with sprite sheet

---
+++ index.js
@@ -0,0 +1,22 @@
+// Pega o canvas da página
+const canvas = document.getElementById("game");
+
+// Permite desenhar no canvas
+const ctx = canvas.getContext("2d");
+
+// Cria uma imagem
+const spriteSheet = new Image();
+
+// Diz onde está a imagem
+spriteSheet.src = "assets/spritesheet.png";
+
+// Quando a imagem terminar de carregar...
+spriteSheet.onload = function () {
+
+    // Limpa o canvas
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Desenha a imagem inteira
+    ctx.drawImage(spriteSheet, 0, 0);
+
+};
